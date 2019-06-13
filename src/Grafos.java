1896dbda4abd8984d8cd554e5d5b361dc40acd50
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2019-06-13 10:38:44 -0300

    Update Grafos.java
    while loop pedindo o vertice inicial agora para corretamente quando o valor é -1

--- src/Grafos.java
+++ src/Grafos.java
@@ -15,15 +15,16 @@ public class Grafos {
         Util util = new Util();
         if(util.initDado()){
             MatrizAdjPeso matrizAdjPeso = new MatrizAdjPeso(util.getListaArestas(), util.getQuantidadeVertices());
-            int opcao;
+            int opcao = 0;
             Scanner scanner = new Scanner(System.in);
             BellmanFord bellmanFord = new BellmanFord(matrizAdjPeso);
 
-            do {
+            while (true){
                 System.out.println("Vertice de origem: ");
                 opcao = scanner.nextInt();
+                if(opcao == -1) return;
                 bellmanFord.bellmanford(opcao);
-            } while (opcao != -1);
+            }
         }
     }
 }
